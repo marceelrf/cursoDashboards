ae2aaabcb7b2d846b779204be92c5000f4bd2241
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2022-07-12 10:12:41 -0400

    ex meu segundo app

--- exercicios/011-meu-segundo-app.R
+++ exercicios/011-meu-segundo-app.R
@@ -10,3 +10,35 @@
 # Observação: se você usar uma base própria, 
 # não se esqueça de descrever as variáveis utilizadas na hora 
 # de tirar dúvidas.
+library(shiny)
+library(ggplot2)
+library(dplyr)
+dado <- readr::read_rds(file = here::here("dados/credito.rds"))
+
+
+ui <- fluidPage(
+  varSelectInput(inputId = "var_num",
+                 data = select(dado,where(is.numeric)),
+                 label = "Numerica"),
+  varSelectInput(inputId = "var_cat",
+                 data = select(dado,where(is.character)),
+                 label = "Categorica",selected = "status"),
+  plotOutput(outputId = "grafico")
+)
+# A minha ideia foi produzir uma visualizção onde o usuario entrasse com uma var numerica
+# sobre o crédito que seria comparada com o status da pessoa, e variaveis categoricas
+# poderia ser utilizadas para colorir de modo a melhorar a visualização. Num contexto real
+# a visualização deveria ser aprimorada.
+server <- function(input, output, session) {
+  
+  output$grafico <- renderPlot({
+    
+    dado %>% 
+      ggplot(aes(x = !!input$var_num, fill = !!input$var_cat)) +
+      geom_histogram() +
+      facet_wrap(~status)
+    
+  })
+}
+
+shinyApp(ui, server)
