cc85caa52e3ee15232f5778064a06cc8e0d2eb29
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2022-07-12 11:41:19 -0400

    ex input pokemon

--- exercicios/020-inputs-outputs-credito.R
+++ exercicios/020-inputs-outputs-credito.R
@@ -3,3 +3,32 @@
 # Utilizando a base de crédito, faça um Shiny app que permite escolher
 # o estado civil, tipo de moradia e/ou trabalho e mostra uma visualização
 # representando a proporção de clientes "bons" e "ruins" da base.
+
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+dado <- readr::read_rds(file = here::here("dados/credito.rds"))
+
+ui <- fluidPage(
+  selectInput(inputId = "estCivil",
+              label = "Estado civil",
+              choices = unique(pull(dado,estado_civil))),
+  selectInput(inputId = "moradia",
+              label = "Moradia",
+              choices = unique(pull(dado,moradia))),
+  selectInput(inputId = "trabalho",
+              label = "Trabalho",
+              choices = unique(pull(dado,trabalho))),
+  plotOutput(outputId = "grafico")
+)
+
+server <- function(input, output, session) {
+  
+  output$grafico <- renderPlot({
+    dado %>% 
+      ggplot(aes(x = ,y = , fill = status))
+  })
+}
+
+shinyApp(ui, server)
--- exercicios/021-inputs-outputs-pokemon.R
+++ exercicios/021-inputs-outputs-pokemon.R
@@ -3,3 +3,32 @@
 # Utilizando a base de pokemon, faça um Shiny app que permite escolher
 # um tipo (tipo_1) e uma geração e faça um gráfico de dispersão do ataque 
 # vs defesa para os pokemon com essas características.
+
+library(shiny)
+library(ggplot2)
+library(dplyr)
+
+pokemon <- readr::read_rds(file = here::here("dados/pkmn.rds"))
+
+ui <- fluidPage(
+  selectInput(inputId = "tipo",
+              label = "Tipo do Pokemon",
+              choices = unique(pull(pokemon,tipo_1))),
+  plotOutput(outputId = "grafico")
+)
+
+server <- function(input, output, session) {
+  
+  
+  output$grafico <- renderPlot({
+    
+    pokemon %>% 
+      filter(tipo_1 == !!input$tipo) %>% 
+      ggplot(aes(x = defesa, y = ataque)) +
+      geom_point()
+    
+  })
+  
+}
+
+shinyApp(ui, server)
