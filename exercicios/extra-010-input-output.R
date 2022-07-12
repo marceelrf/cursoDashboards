c053f55e15d1ceaf2d658916d3367a7551f305a5
Marcel Ferreira <marcelrodrigues45@yahoo.com.br>
2022-07-12 10:20:20 -0400

    ex extra input-output

--- exercicios/extra-010-input-output.R
+++ exercicios/extra-010-input-output.R
@@ -9,3 +9,26 @@
 #     a ser colocada no eixo y
 #
 #   - Um output: um gráfico de dispersão
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+ui <- fluidPage(
+  varSelectInput(inputId = "var_x",label = "Variavel X",data = select(mtcars,where(is.numeric))),
+  varSelectInput(inputId = "var_y",label = "Variavel Y",data = select(mtcars,where(is.numeric))),
+  plotOutput(outputId = "grafico")
+)
+
+server <- function(input, output, session) {
+  
+  output$grafico <- renderPlot({
+    
+    mtcars %>% 
+      ggplot(aes(x = !!input$var_x,y = !!input$var_y)) +
+      geom_point(size = 2)
+    
+  })
+  
+}
+
+shinyApp(ui, server)
